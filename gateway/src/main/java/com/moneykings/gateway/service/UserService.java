980f5c8d74238b0ea27e9797fce60b1f9ad27a34
vilion <yangyangyongyuan@vip.qq.com>
2026-01-17 21:43:06 +0800

    初始化提交

--- gateway/src/main/java/com/moneykings/gateway/service/UserService.java
+++
@@ -1,8 +0,0 @@
-package com.moneykings.gateway.service;
-
-
-import com.moneykings.gateway.entity.User;
-
-public interface UserService {
-    public User findByUserName(String email);
-}
--- user/src/main/java/com/moneykings/user/dao/UserVO.java
+++
@@ -1,9 +0,0 @@
-package com.moneykings.user.dao;
-
-import lombok.Data;
-
-import java.util.List;
-@Data
-public class UserVO extends User{
-    private List<Role> roles;
-}
